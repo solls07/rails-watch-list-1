a7cb1231602b6228ebbf0f131f713ff3f7d20137
Laura Sol <138508411+solls07@users.noreply.github.com>
2023-09-18 16:55:25 +0200

    1st push

---
+++ app/models/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :movie, presence: true
+  validates :list, presence: true
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'This movie is already in the list.' }
+end
---
+++ app/models/list.rb
@@ -0,0 +1,8 @@
+class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+
+
+  validates :name, presence: true, uniqueness: true
+end
